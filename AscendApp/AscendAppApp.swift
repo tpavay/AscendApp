8b9b1fe3d1e534534e49de0d2d1de88293576909
tpavay <pavayt@gmail.com>
2025-07-13 11:23:38 -0400

    Initial Commit

---
+++ AscendApp/AscendAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AscendAppApp.swift
+//  AscendApp
+//
+//  Created by Tyler Pavay on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AscendAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
